fce9bf0942f4b9193b0eefec72628d83768712e0
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-01-25 15:16:24 +0900

    initial commit

---
+++ string_maipulation_01.py
@@ -0,0 +1,26 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+import typing import *
+
+
+def isPalindrome(s: str) -> bool:
+    strs = []
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    # 팰린드롬 여부 판별
+    while len(strs) > 1:
+        if strs.pop(0) != strs.pop():
+            return False
+
+    return True
+
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
