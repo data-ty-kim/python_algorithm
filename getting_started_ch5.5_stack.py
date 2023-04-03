583d31f77ad77e076131202afc0fbd0097658591
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-04-03 11:46:48 +0900

    initial commit

---
+++ getting_started_ch5.5_stack.py
@@ -0,0 +1,18 @@
+# 5.5 힙 정렬
+
+stack = []
+
+stack.append(3)
+stack.append(5)
+stack.append(2)
+
+temp = stack.pop()
+print(temp)
+
+temp = stack.pop()
+print(temp)
+
+stack.append(4)
+
+temp = stack.pop()
+print(temp)
