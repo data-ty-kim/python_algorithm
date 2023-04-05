dd19643bf4f277654a4d0be2bacfd746e6b8f88d
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-04-05 14:53:41 +0900

    initial commit

--- getting_started_with_python_algorithm/getting_started_ch5.5_heap_sort1.py
+++ getting_started_with_python_algorithm/getting_started_ch5.5_heap_sort1.py
@@ -13,4 +13,19 @@ for i in range(len(data)):
 # 정렬 실행
 for i in range(len(data), 0, -1):
     # 힙의 맨 앞과 교환
-    data[i - 1], data[0] = data[0], data[i -]
+    data[i - 1], data[0] = data[0], data[i - 1]
+    j = 0
+    while ((2 * j + 1 < i - 1) and (data[j] < data[2 * j + 1])) \
+        or ((2 * j + 2 < i - 1) and (data[j] < data[2 * j + 2])):
+        if (2 * j + 2 == i - 1) or (data[2 * j + 1] > data[2 * j + 2]):
+            # 왼쪽 아래와 교환
+            data[j], data[2 * j + 1] = data[2 * j + 1], data[j]
+            # 왼쪽 아래로 이동
+            j = 2 * j + 1
+        else:
+            # 오른쪽 아래와 교환
+            data[j], data[2 * j + 2] = data[2 * j + 2], data[j]
+            # 오른쪽 아래로 이동
+            j = 2 * j + 2
+
+print(data)        
