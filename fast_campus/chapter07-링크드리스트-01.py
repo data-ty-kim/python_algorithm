5010139dbc1b680528f759941bb1ea19d2b60b10
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-10-13 17:20:04 +0900

    연결리스트

---
+++ fast_campus/chapter07-링크드리스트-01.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def add(data):
+    node = head
+    while node.next:
+        node = node.next
+    node.next = Node(data)
+
+
+# 예제: Node와 Node 연결하기
+node1 = Node(1)
+node2 = Node(2)
+node1.next = node2
+head = node1
+
+# 예제: 링크드 리스트로 데이터 추가하기
+node1 = Node(1)
+head = node1
+for index in range(2, 10):
+    add(index)
+
+# 예제: 링크드 리스트 데이터 출력하기
+node = head
+while node.next:
+    print(node.data)
+    node = node.next
+print(node.data)
