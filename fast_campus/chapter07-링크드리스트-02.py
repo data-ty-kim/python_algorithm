79567421a28ad7dc0c24e3cf7c94ca35e11ed949
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-10-17 10:47:46 +0900

    링크드리스트리스트

--- fast_campus/chapter07-링크드리스트-02.py
+++ fast_campus/chapter07-링크드리스트-02.py
@@ -24,12 +24,61 @@ class NodeMgmt:
             print(node.data)
             node = node.next
 
+    def delete(self, data):
+        if self.head == '':
+            print("해당 값을 가진 노드가 없습니다.")
+            return
+
+        if self.head.data == data:
+            temp = self.head
+            self.head = self.head.next
+            del temp
+        else:
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+                    return
+                else:
+                    node = node.next
+
 
 # 링크드리스트 만들어보기
+print('\n링크드리스트 만들어보기')
 linkedlist1 = NodeMgmt(0)
 linkedlist1.desc()
 
 # 데이터 추가해보기
+print('\n데이터 추가해보기')
 for data in range(1, 10):
     linkedlist1.add(data)
+linkedlist1.desc()
+
+
+# 테스트를 위해 1개 노드를 만들어 봄
+print('\n테스트')
+linkedlist1 = NodeMgmt(0)
+linkedlist1.desc()
+
+# head가 살아있음을 확인
+print(linkedlist1.head)
+# head를 지워 봄 (경우의 수 1)
+linkedlist1.delete(0)
+print(linkedlist1.head)
+
+# 이번에는 여러 노드를 더 추가해봄
+linkedlist1 = NodeMgmt(0)
+linkedlist1.desc()
+for data in range(1, 10):
+    linkedlist1.add(data)
+linkedlist1.desc()
+
+# 노드 중에 한 개를 삭제해 봄 (경우의 수 2)
+linkedlist1.delete(4)
+linkedlist1.desc()
+
+# 마지막 노드를 삭제해 봄 (경우의 수 3)
+linkedlist1.delete(9)
 linkedlist1.desc()
