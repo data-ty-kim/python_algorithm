8326edba93e9131fa3bc0dfb005862b4c6033184
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-11-24 17:33:10 +0900

    이진 탐색 트리 삭제 코드 경우 3-1 수정

--- fastcampus/chapter10-트리-02.py
+++ fastcampus/chapter10-트리-02.py
@@ -1,7 +1,46 @@
 # 이진 탐색 트리 삭제 코드 구현과 분석
 # 머리 속으로 상상해서 코드 작성하지 말고 종이에 그림을 그려 작성한 뒤 코드를 작성하라
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class NodeMgmt:
+    def __init__(self, head):
+        self.head = head
+
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left != None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+
+    def search(self, value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+        return False
+
     def delete(self, value):
+        # 삭제할 Node 탐색
         searched = False
         self.current_node = self.head
         self.parent = self.head
@@ -16,11 +55,13 @@
                 self.parent = self.current_node
                 self.current_node = self.current_node.right
 
+        # 삭제할 Node가 없는 경우는 False를 반환하고 함수를 종료
         if searched == False:
             return False
 
         # 이후부터 case를 분리해서 코드 작성
-        # case1: self.current_node가 삭제할 Node,
+        # case1: 삭제할 Node가 Leaf Node인 경우
+        #        self.current_node가 삭제할 Node,
         #        self.parent는 삭제할 Node의 Parent Node인 상태
         if self.current_node.left == None and self.current_node.right == None:
             if value < self.parent.value:
@@ -41,18 +82,21 @@
             else:
                 self.parent.right = self.current_node.right
 
-        # case3:
+        # case3: 삭제할 Node가 Child Node를 두 개 가지고 있을 경우
         if self.current_node.left != None and self.current_node.right != None:
             # case 3-1: 삭제할 Node가 Parent Node 왼쪽에 있을 때
             if value < self.parent.value:
                 self.change_node = self.current_node.right
                 self.change_node_parent = self.current_node.right
                 while self.change_node.left != None:
-                    self.change_node_parent = self.change_node_parent
+                    self.change_node_parent = self.change_node
                     self.change_node = self.change_node.left
-                self.change_node_parent.left = None
+                # case 3-1-2: 삭제할 Node의 오른쪽 자식 중,
+                #             가장 작은 값을 가진 Node의 오른쪽에 Child Node가 있을 때
                 if self.change_node.right != None:
                     self.change_node_parent.left = self.change_node.right
+                # case 3-1-1: 삭제할 Node의 오른쪽 자식 중,
+                #             가장 작은 값을 가진 Node의 Child Node가 없을 때
                 else:
                     self.change_node_parent.left = None
                 self.parent.left = self.change_node
